90160e4f4aa0b0e9ad552199b90f4f1afe1b81bf
Exodus <kubagr2000@gmail.com>
2022-11-09 11:52:55 +0100

    Fix output weight being 1 less than solution

--- TravelingSalesmanProblem/Algorithms/BruteForce.cs
+++ TravelingSalesmanProblem/Algorithms/BruteForce.cs
@@ -9,60 +9,65 @@ public class BruteForce : ITspAlgorithm
   {
     var outputWeight = int.MaxValue;
     var outputPath = new List<int>();
-    var vertices = new List<int>();
+    var permutation = new List<int>();
     
     // dodaj wszystkie krawedzie oprocz krawedzi startowej
-    for (var i = 0; i < graph.Size; i++)
+    for(var i = 0; i < graph.Size; i++)
     {
       if (i != start)
       {
-        vertices.Add(i);
+        permutation.Add(i);
       }
     }
 
     var hasNextPermutation = true;
-    while (hasNextPermutation)
+    while(hasNextPermutation)
     {
       var currentVertex = start;
       
-      // zsumowanie wag obecnej permutacji
-      var currentWeight = graph.AdjacencyMatrix[currentVertex][start];
-      foreach (var vertex in vertices)
+      var currentWeight = 0;
+      // zsumowanie wag krawedzi
+      foreach (var vertex in permutation)
       {
         currentWeight += graph.AdjacencyMatrix[currentVertex][vertex];
         currentVertex = vertex;
       }
+      // dodanie wagi krawedzi z ostatniego wierzcholka ostatniego do startu sciezki
       currentWeight += graph.AdjacencyMatrix[currentVertex][start];
-      
+
       // ustawienie wagi obecnie przeliczonej drogi jesli jest mniejsza od poprzedniej
       if(outputWeight > currentWeight)
       {
         outputWeight = currentWeight;
-        outputPath = new List<int>(vertices);
+        outputPath = new List<int>(permutation);
         outputPath.Insert(0, start);
         outputPath.Add(start);
       }
 
       // ustawienie nowej permutacji (jesli istnieje)
-      hasNextPermutation = FindNextPermutation(vertices);
+      hasNextPermutation = FindNextPermutation(permutation);
     }
     return (outputWeight, outputPath);
   }
   
+  // generowanie następnej permutacji poprzez porzadek leksykograficzny
   private static bool FindNextPermutation(IList<int> input)
   {
     if(input.Count <= 1) return false;
     var i = input.Count - 2;
-    
-    while(i >= 0 && input[i] >= input[i + 1]) i--;
-    
+
+    while (i >= 0 && input[i] >= input[i + 1])
+    {
+      i--;
+    }
+
     if (i < 0)
       return false;
-    
-    var j = input.Count - 1;              
-    while(input[j] <= input[i]) j--;      
-    
-    input.Swap(i, j);                     
+
+    var j = input.Count - 1;
+    while (input[j] <= input[i]) j--;
+
+    input.Swap(i, j);
     input.ReverseSubList(i + 1, input.Count - 1);
     return true;
   }
